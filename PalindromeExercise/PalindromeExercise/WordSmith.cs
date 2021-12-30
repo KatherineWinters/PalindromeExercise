788d8c9ba0f84f9e67787189406f3d8a62806671
Katherine Winters <goble.katherine@gmail.com>
2021-12-29 23:07:43 -0600

    inital commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalidrome(string word)
+        {
+            int words = word.Length - 1;
+            for (int i = 0; i < word.Length / 2; i++)
+            {
+                if (word[i] != word[words - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -5,10 +5,18 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("flower", false)]
+        [InlineData("jeans", false)]
+        public void Test1(string word, bool expected)
         {
+            
+            var wordSmith = new PalindromeExercise.WordSmith();
 
+            var actual = wordSmith.IsAPalidrome(word);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
